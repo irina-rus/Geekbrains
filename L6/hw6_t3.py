6846a11c5a0f445aa922335e732e9c850f9e0580
Irina <irinakostiukhina@gmail.com>
2020-11-23 20:43:22 +0300

    [upd] ДЗ - Урок6

---
+++ L6/hw6_t3.py
@@ -0,0 +1,32 @@
+#Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+# income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus}. Создать класс Position (должность) на базе класса Worker.
+# В классе Position реализовать методы получения полного имени сотрудника (get_full_name) и
+# дохода с учетом премии (get_total_income). Проверить работу примера на реальных данных
+# (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        print(f'Name and surname - {self.name} {self.surname}')
+        return
+
+    def get_total_income(self):
+        print(f'Position - {self.position}')
+        print(f'Total income = {sum(self._income.values())}')
+        return
+
+p = Position('John', 'Johnson', 'Data Scientist', 5000, 5000)
+p.get_full_name()
+p.get_total_income()
